0f5d0999d4f82bb11289a5a3e75d902672be9453
Nikita Pochaev <pochaev.nik@gmail.com>
2022-02-28 23:33:34 +0300

    Update dependencies

--- app/src/main/java/com/dwards/a5edpockethelper/MyViewModelFactory.kt
+++ app/src/main/java/com/dwards/a5edpockethelper/MyViewModelFactory.kt
@@ -5,11 +5,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.dwards.a5edpockethelper.model.CharacterDAO
 
-class MyViewModelFactory(private val characterDao: CharacterDAO, val app: Application) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+class MyViewModelFactory(private val characterDao: CharacterDAO, val app: Application) :
+    ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MyViewModel::class.java)) {
             return MyViewModel(characterDao, app) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
+
 }
